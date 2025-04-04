b23d0cd858d998cd7954b0dcd7ee372cc2e0251c
Juan Camilo López Morales <camilopezm24734m@gmail.com>
2025-04-04 18:19:39 -0500

    feat(ConfiarApplicationTests): clase de pruebas

---
+++ src/test/java/co/com/confiar/ConfiarApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.confiar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
